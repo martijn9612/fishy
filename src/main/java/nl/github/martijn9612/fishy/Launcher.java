69bf7acfd87839ad2438f29aa4925eb70e0b1b9f
martijn9612 <m.g.a.janssen@student.tudelft.nl>
2015-09-05 17:07:32 +0200

    Added Opening Of First Screen

--- src/main/java/nl/github/martijn9612/fishy/Launcher.java
+++ src/main/java/nl/github/martijn9612/fishy/Launcher.java
@@ -1,7 +1,38 @@
 package nl.github.martijn9612.fishy;
 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.stage.Stage;
+
 /**
- * Created by martijn on 5-9-15.
+ * Creates the fishy game and launches it
  */
-public class Launcher {
+public class Launcher extends Application {
+    /**
+     * Starts the game
+     *
+     * @param args Are not used
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * Starts the initial screen for the game, the welcome screen
+     *
+     * @param primaryStage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Fishy");
+        Group root = new Group();
+        Canvas canvas = new Canvas(300, 300);
+        root.getChildren().add(canvas);
+        Scene startScreen = new Scene(root);
+        primaryStage.setScene(startScreen);
+        primaryStage.show();
+    }
 }
