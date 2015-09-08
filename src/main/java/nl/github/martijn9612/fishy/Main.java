22eb2b21200fd5f9504feb5f19552a134a7323fd
MilleIT <m.j.e.vanosch@student.tudelft.nl>
2015-09-08 12:22:45 +0200

    checkstyle fixwa

--- src/main/java/nl/github/martijn9612/fishy/Main.java
+++ src/main/java/nl/github/martijn9612/fishy/Main.java
@@ -2,14 +2,11 @@ package nl.github.martijn9612.fishy;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.BasicGame;
 
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
-import org.newdawn.slick.tests.StateBasedTest;
 
 public class Main extends StateBasedGame
 {
