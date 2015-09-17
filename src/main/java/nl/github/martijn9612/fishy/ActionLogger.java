ce08511443b2202547071d8f6bf6d66258649755
martijn9612 <m.g.a.janssen@student.tudelft.nl>
2015-09-17 21:35:49 +0200

    Remove Complicated Loop In Constructor

--- src/main/java/nl/github/martijn9612/fishy/ActionLogger.java
+++ src/main/java/nl/github/martijn9612/fishy/ActionLogger.java
@@ -2,7 +2,6 @@ package nl.github.martijn9612.fishy;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -12,26 +11,10 @@ import java.util.Date;
 public class ActionLogger {
     private static FileWriter fileWriter;
     private static SimpleDateFormat dateFormat = new SimpleDateFormat();
-    private int logFileNumber = 1;
 
     ActionLogger() {
-        boolean fileExists;
         try {
             fileWriter = new FileWriter("log.txt", false);
-        } catch (FileAlreadyExistsException FileExistsException) {
-            System.out.println("log.txt already exists!");
-            fileExists = true;
-            while (fileExists) {
-                try {
-                    fileWriter = new FileWriter("log(" + logFileNumber + ").txt", false);
-                    fileExists = false;
-                } catch (FileAlreadyExistsException fileExistsException) {
-                    System.out.println("log(" + logFileNumber + ").txt already exists!");
-                    logFileNumber++;
-                } catch (Exception e) {
-                    System.out.println(e.toString());
-                }
-            }
         } catch (Exception e) {
             System.out.println(e.toString());
         }
@@ -39,7 +22,7 @@ public class ActionLogger {
 
     public void logLine(String text, String className) {
         try {
-            fileWriter.write("[ " + getTimeStamp() + "] - " + className + " - " + text + "\n");
+            fileWriter.write("[ " + getTimeStamp() + "] - " + text + " - " + className + "\n");
         } catch (IOException e) {
             System.out.println("Unable to log to file!");
             e.printStackTrace();
@@ -49,7 +32,7 @@ public class ActionLogger {
     public void logLine(String text, String className, boolean isError) {
         try {
             if (isError) {
-                fileWriter.write("[ERROR!][ " + getTimeStamp() + "] - " + className + " - " + text + "\n");
+                fileWriter.write("[ERROR!][ " + getTimeStamp() + "] - " + text + " - " + className + "\n");
             } else {
                 logLine(text, className);
             }
