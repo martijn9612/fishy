22353d1ab1e418702c3dfc6832ffed0241ff6b5f
Leon Noordam <leonnoordam@gmail.com>
2015-09-17 22:19:53 +0200

    Add background music in the menu and play states <feature

--- src/main/java/nl/github/martijn9612/fishy/LevelState.java
+++ src/main/java/nl/github/martijn9612/fishy/LevelState.java
@@ -5,6 +5,7 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -18,6 +19,7 @@ public class LevelState extends BasicGameState {
     public static String score =  "0";
     private OpponentHandler opponentHandler;
     public String fishPosition = "(" + 0 + "," + 0 + ")";
+	public Sound bgPlayMusic;
 
   public LevelState(int state) {
 
@@ -30,7 +32,14 @@ public class LevelState extends BasicGameState {
     player = new Player();
     background = new Image("resources/" + Main.LEVEL_BACKGROUND + ".jpg");
     opponentHandler = new OpponentHandler();
+    bgPlayMusic = new Sound("resources/sounds/bg-play-music.wav");
   }
+  
+	@Override
+	public void enter(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+		super.enter(gameContainer, stateBasedGame);
+		bgPlayMusic.loop();
+	}
 
   /**
    * render everything in the playscreen.
@@ -59,6 +68,12 @@ public class LevelState extends BasicGameState {
     opponentHandler.newOpponent(player);
     fishPosition = "(" + player.x + "," + player.y + ")";
   }
+  
+	@Override
+	public void leave(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+		super.leave(gameContainer, stateBasedGame);
+		bgPlayMusic.stop();
+	}
 
   /**
    * get id.
