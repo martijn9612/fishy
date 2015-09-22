af72f0633da661f25783103042e193a333c688ac
Fleaxes <D.L.M.lummen@student.tudelft.nl>
2015-09-22 17:56:31 +0200

    checkstyle fixes for LoseState.java <documentation,layout,naming

--- src/main/java/nl/github/martijn9612/fishy/LoseState.java
+++ src/main/java/nl/github/martijn9612/fishy/LoseState.java
@@ -13,6 +13,9 @@ import nl.github.martijn9612.fishy.models.DrawRectangle;
 import nl.github.martijn9612.fishy.models.MousePosition;
 import nl.github.martijn9612.fishy.models.MouseRectangle;
 
+/**
+ * Implements the Lose State of the game.
+ */
 public class LoseState extends BasicGameState {
 	private Image play;
 	private Image deadFish;
@@ -20,30 +23,48 @@ public class LoseState extends BasicGameState {
 	private DrawRectangle playButtonDR;
 	private MouseRectangle playButtonMR;
 	
-	private static int PLAY_BUTTON_DRAW_X = 150;
-	private static int PLAY_BUTTON_DRAW_Y = 350;
-	private static int DEAD_FISH_DRAW_X = 220;
-	private static int DEAD_FISH_DRAW_Y = 150;
-	private static int DEAD_TEXT_DRAW_X = 230;
-	private static int DEAD_TEXT_DRAW_Y = 100;
+	private static final int PLAY_BUTTON_DRAW_X = 150;
+	private static final int PLAY_BUTTON_DRAW_Y = 350;
+	private static final int DEAD_FISH_DRAW_X = 220;
+	private static final int DEAD_FISH_DRAW_Y = 150;
+	private static final int DEAD_TEXT_DRAW_X = 230;
+	private static final int DEAD_TEXT_DRAW_Y = 100;
 	
-	private static String DEAD_TEXT_STRING = "Urr a dead fish! :(";
-	private static String PLAY_BUTTON_RESOURCE = "resources/play-button.gif";
-	private static String DEAD_FISH_RESOURCE = "resources/dead-fish.png";
+	private static final String DEAD_TEXT_STRING = "Urr a dead fish! :(";
+	private static final String PLAY_BUTTON_RESOURCE = "resources/play-button.gif";
+	private static final String DEAD_FISH_RESOURCE = "resources/dead-fish.png";
 	
+    /**
+     * Constructor for the Lose State.
+     * @param state - the number of the state
+     */
 	public LoseState(int state) {
 		// Blank
 	}
 
+	/**
+     * Initialize the game.
+     * @param container - the container holding the game
+     * @param game - the game holding the state
+     * @throws SlickException - indicates internal error
+     */
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
 		play = new Image(PLAY_BUTTON_RESOURCE);
 		deadFish = new Image(DEAD_FISH_RESOURCE);
-		playButtonDR = new DrawRectangle(PLAY_BUTTON_DRAW_X, PLAY_BUTTON_DRAW_Y, play.getWidth(), play.getHeight());
+		playButtonDR = new DrawRectangle(PLAY_BUTTON_DRAW_X, PLAY_BUTTON_DRAW_Y, 
+		        play.getWidth(), play.getHeight());
 		playButtonMR = playButtonDR.getMouseRectangle();
 		mouse = new MousePosition();
 	}
 
-	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+    /**
+     * Renders the game's screen.
+     * @param gc - the container holding the game
+     * @param game - the game holding the state
+     * @param g - the graphics content used to render
+     * @throws SlickException - indicates internal error
+     */
+	public void render(GameContainer gc, StateBasedGame game, Graphics g) throws SlickException {
 		g.setColor(Color.white);
 		g.fillRect(0, 0, Main.WINDOW_WIDTH, Main.WINDOW_HEIGHT);
 		g.drawImage(play, playButtonDR.getPositionX(), playButtonDR.getPositionY());
@@ -52,21 +73,32 @@ public class LoseState extends BasicGameState {
 		g.drawImage(deadFish, DEAD_FISH_DRAW_X, DEAD_FISH_DRAW_Y);
 	}
 
-	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+	/**
+     * Update the game logic.
+     * @param gc - the container holding the game
+     * @param game - the game holding the state
+     * @param delta - the amount of time that has passed since last update in milliseconds
+     * @throws SlickException - indicates internal error
+     */
+	public void update(GameContainer gc, StateBasedGame game, int delta) throws SlickException {
         mouse.updatePosition();
         
-        if(mouse.isInRectangle(playButtonMR)) {
-        	if(mouse.isLeftButtonDown()) {
+        if (mouse.isInRectangle(playButtonMR)) {
+        	if (mouse.isLeftButtonDown()) {
         		game.enterState(Main.LEVEL_STATE);
         	}
         }
         
-        Input input = container.getInput();
-        if(input.isKeyDown(Input.KEY_ENTER)) {
+        Input input = gc.getInput();
+        if (input.isKeyDown(Input.KEY_ENTER)) {
         	game.enterState(Main.LEVEL_STATE);
         }
 	}
 
+    /**
+     * Get the ID of this state.
+     * @return the unique ID of this state
+     */
 	@Override
 	public int getID() {
 		return Main.GAME_LOSE_STATE;
