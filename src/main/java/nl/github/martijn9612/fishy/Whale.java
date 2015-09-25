9d81fb0eb43d305394d41e74ca4827b3a69f5a8c
skullyhoofd <ricardojongerius@gmail.com>
2015-09-25 16:44:40 +0200

    Creation of the whale (the big fish) class and addition of its sprite <feature

---
+++ src/main/java/nl/github/martijn9612/fishy/Whale.java
@@ -0,0 +1,30 @@
+package nl.github.martijn9612.fishy;
+
+/**
+ * The whale class. The whale is a special opponent which has a small chance to spawn and takes up a large portion of the screen.
+ * Created by Skullyhoofd on 25/09/2015.
+ */
+public class Whale extends Entity {
+
+    private int yPos;
+    private String IMAGE_NAME = "whale";
+    private int xPos = 1000;
+    private double size = 200;
+    private int speed = 3;
+
+
+    public Whale( int playery) {
+        this.yPos = playery;
+        this.loadImage(IMAGE_NAME);
+        this.setPosition(xPos, yPos);
+        this.setDimensions((int) Math.round(this.size), (int) Math.round(this.size));
+        this.setSpeed(speed);
+        this.calculateInitialBoundingbox();
+    }
+
+    
+
+
+
+
+}
