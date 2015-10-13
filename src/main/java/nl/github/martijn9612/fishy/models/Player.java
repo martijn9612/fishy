14e4d2a7dd1bd994a8626071faf2e4519f5e5da3
martijn9612 <m.g.a.janssen@student.tudelft.nl>
2015-10-13 18:12:59 +0200

    Create Setters And Getters For Levelstate Variables <layout

--- src/main/java/nl/github/martijn9612/fishy/models/Player.java
+++ src/main/java/nl/github/martijn9612/fishy/models/Player.java
@@ -180,7 +180,7 @@ public class Player extends Entity {
      */
     public void resetPlayerVariables() {
         Main.actionLogger.logLine("Player resetted", getClass().getSimpleName());
-        Main.actionLogger.logLine("Score was " + LevelState.score, getClass().getSimpleName());
+        Main.actionLogger.logLine("Score was " + LevelState.getScore(), getClass().getSimpleName());
         position = Vector.centerOfScreen();
         dimensions = new Vector(PLAYER_WIDTH, PLAYER_HEIGHT);
         PLAYER_MAX_SPEED = 8;
@@ -225,7 +225,7 @@ public class Player extends Entity {
      * @param score new score value
      */
     public void setScore(double score) {
-        LevelState.score = String.valueOf(Math.round(score));
+        LevelState.setScore(String.valueOf(Math.round(score)));
         this.score = score;
     }
 
