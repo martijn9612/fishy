e432bd085d932bbd3ba47f572e2534e68ef3625a
skullyhoofd <ricardojongerius@gmail.com>
2015-10-13 23:34:46 +0200

    fix build error <bug

--- src/main/java/nl/github/martijn9612/fishy/states/HelpState.java
+++ src/main/java/nl/github/martijn9612/fishy/states/HelpState.java
@@ -1,7 +1,11 @@
 package nl.github.martijn9612.fishy.states;
 
 import nl.github.martijn9612.fishy.Main;
+import nl.github.martijn9612.fishy.position.DrawRectangle;
+import nl.github.martijn9612.fishy.position.MousePosition;
+import org.newdawn.slick.*;
 import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
 
 /**
  * Created by Skullyhoofd on 13/10/2015.
@@ -16,6 +20,53 @@ public class HelpState extends BasicGameState {
      */
     public HelpState(int state) {
         // Blank
+    }
+
+    /**
+     * Initialize the game.
+     *
+     * @param container
+     *            - the container holding the game
+     * @param game
+     *            - the game holding the state
+     * @throws SlickException
+     *             - indicates internal error
+     */
+    public void init(GameContainer container, StateBasedGame game) throws SlickException {
+
+    }
+
+    /**
+     * Renders the game's screen.
+     *
+     * @param gc
+     *            - the container holding the game
+     * @param game
+     *            - the game holding the state
+     * @param g
+     *            - the graphics content used to render
+     * @throws SlickException
+     *             - indicates internal error
+     */
+    public void render(GameContainer gc, StateBasedGame game, Graphics g) throws SlickException {
+
+    }
+
+    /**
+     * Update the game logic.
+     *
+     * @param gc
+     *            - the container holding the game
+     * @param game
+     *            - the game holding the state
+     * @param delta
+     *            - the amount of time that has passed since last update in
+     *            milliseconds
+     * @throws SlickException
+     *             - indicates internal error
+     */
+    public void update(GameContainer gc, StateBasedGame game, int delta) throws SlickException {
+        
     }
 
 
