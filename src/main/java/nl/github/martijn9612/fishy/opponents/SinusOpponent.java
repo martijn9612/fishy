35540b2bd4158f16386902189c12beb1b3ea3c67
Fleaxes <D.L.M.lummen@student.tudelft.nl>
2015-10-20 11:15:17 +0200

    Updated Javadoc for SinusOpponent class <documentation

--- src/main/java/nl/github/martijn9612/fishy/opponents/SinusOpponent.java
+++ src/main/java/nl/github/martijn9612/fishy/opponents/SinusOpponent.java
@@ -11,7 +11,10 @@ import nl.github.martijn9612.fishy.models.Player;
 import nl.github.martijn9612.fishy.models.Vector;
 
 /**
- * Created by martijn on 23-9-15.
+ * Implementation of the SinusOpponent class. 
+ * A SinusOpponent is a small opponent that moves
+ * vertically through the map in a sinusoid motion.
+ * Software Engineering Methods Project - Group 11.
  */
 public class SinusOpponent extends NonPlayer {
     private static final String SINUS_SPRITE_PATH = "resources/squid.png";
@@ -22,13 +25,9 @@ public class SinusOpponent extends NonPlayer {
 
     /**
 	 * Constructor for an opponent that moves in a sinusoid motion.
-	 * 
-	 * @param dimensions size of the new opponent.
-	 * @param position vector with the start position of the opponent.
-	 * @param velocity initial speed of the opponent.
-	 * @param acceleration initial acceleration of the opponent.
-	 * @param loadResources whether the sprite resources should be loaded.
-	 */
+     * @param data - the moveable data the SinusOpponent will have.
+     * @param loadResources - true when OpenGL context should be loaded, false if not.
+     */
     public SinusOpponent(Moveable data, boolean loadResources) {
         super(data, loadResources);
         loadResources(SINUS_SPRITE_PATH);
@@ -36,10 +35,10 @@ public class SinusOpponent extends NonPlayer {
     
     /**
 	 * Creates an instance of SinusOpponent at the bottom of the screen.
-	 * 
-	 * @param player an instance of the Player class.
-	 * @param random an instance to generate random numbers.
-	 * @param loadResources whether the sprite resources should be loaded.
+     * @param player - the current Player in the game.
+     * @param random - an instance to generate random numbers.
+     * @param loadResources - true when OpenGL context should be loaded, false if not.
+     * @return new SinusOpponent
 	 */
 	public static SinusOpponent createRandom(Player player, Random random, boolean loadResources) {
 		Moveable data = new Moveable();
@@ -48,13 +47,12 @@ public class SinusOpponent extends NonPlayer {
 		return new SinusOpponent(data, loadResources);
 	}
 	
-	/**
-	 * Creates a random dimension vector for the SinusOpponent class.
-	 * 
-	 * @param player an instance of the Player class.
-	 * @param random an instance to generate random numbers.
-	 * @return Vector with opponent size.
-	 */
+    /**
+     * Creates a random dimension vector for the SinusOpponent class.
+     * @param player - the current Player in the game.
+     * @param random - an instance to generate random numbers.
+     * @return Vector with opponent size.
+     */
 	private static Vector getRandomDimensions(Player player, Random random) {
 		int maxSize = (int) (player.getSize() * 2.0);
 		int minSize = (int) (player.getSize() * 0.5);
@@ -62,13 +60,12 @@ public class SinusOpponent extends NonPlayer {
 		return new Vector(size, size);
 	}
 	
-	/**
-	 * Creates a random position vector for the SinusOpponent class.
-	 * 
-	 * @param player an instance of the Player class.
-	 * @param random an instance to generate random numbers.
-	 * @return Vector with opponent location.
-	 */
+    /**
+     * Creates a random position vector for the SinusOpponent class.
+     * @param random - an instance to generate random numbers.
+     * @param dimensions - Vector with the dimensions of the LinearOpponent.
+     * @return Vector with opponent location.
+     */
     private static Vector getRandomPosition(Random random, Vector dimensions) {
     	int min = (int) Math.round(dimensions.x);
     	int max = 615 - min;
@@ -78,9 +75,8 @@ public class SinusOpponent extends NonPlayer {
 
 	/**
      * Moves the opponent towards the top of the screen in a sinusoid motion.
-     * 
-     * @param gc the container holding the game.
-     * @param deltaTime time elapsed since method was called in milliseconds.
+     * @param gc - the container holding the game.
+     * @param deltaTime - time elapsed since method was called in milliseconds.
      */
     @Override
     public void objectLogic(GameContainer gc, int deltaTime) {
