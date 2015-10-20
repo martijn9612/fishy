0292fb16b4a6cdc662dd0523ff8df6799a141742
Fleaxes <D.L.M.lummen@student.tudelft.nl>
2015-10-20 11:22:08 +0200

    Updated Javadoc for DrawPosition and DrawRectangle classes <documentation

--- src/main/java/nl/github/martijn9612/fishy/position/DrawPosition.java
+++ src/main/java/nl/github/martijn9612/fishy/position/DrawPosition.java
@@ -12,20 +12,33 @@ import org.newdawn.slick.Image;
  *   |        .   |
  * (0,y) ------ (x,y)
  * 
+ * Implementation of DrawPosition class.
+ * Software Engineering Methods Project - Group 11;
  */
 public class DrawPosition extends Position {
 	
+    /**
+     * Creates a new DrawPosition, based on Position class.
+     * @param positionX - x coordinate of the new DrawPosition.
+     * @param positionY - y coordinate of the new DrawPosition.
+     */
 	public DrawPosition(int positionX, int positionY) {
 		super(positionX, positionY);
 	}
 	
+	/**
+	 * Draws the image on positionX and positionY.
+	 * @param g - graphics content used.
+	 * @param image - image to be drawn.
+	 */
 	public void drawImage(Graphics g, Image image) {
 		g.drawImage(image, positionX, positionY);
 	}
 	
 	/**
-	 * Translates the Draw coordinate system into Mouse coordinates. This is achieved by rotating the y-axis around its center.
-	 * @return {MousePosition}
+	 * Translates the Draw coordinate system into Mouse coordinates. 
+	 * This is achieved by rotating the y-axis around its center.
+	 * @return the MousePosition.
 	 */
 	public MousePosition getMousePosition() {
 		MousePosition position = new MousePosition(positionX, positionY);
@@ -34,11 +47,12 @@ public class DrawPosition extends Position {
 	}
 	
 	/**
-	 * Debug method which formats the contents of the DrawPosition object into a single String.
-	 * @return {String}
+	 * Debug method which formats the contents of 
+	 * the DrawPosition object into a single String.
+	 * @return string representation of the DrawPosition.
 	 */
 	public String toString() {
-		return new String("Draw position X: "+positionX+", Y: "+positionY);
+		return "Draw position X: " + positionX + ", Y: " + positionY;
 	}
 	
 }
